f12a571082a035c66ba65f296e81db178c0b908f
vndrixninx <andrianina252@gmail.com>
2025-08-07 09:57:42 +0300

    Add dependicies

---
+++ mon-Examen/src/main/java/com/Examen/App.java
@@ -0,0 +1,13 @@
+package com.Examen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
